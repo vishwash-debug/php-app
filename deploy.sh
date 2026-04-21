2d6f0bc48c9c69a217addc854952640ef5229008
Ubuntu <ubuntu@ip-172-31-35-150.ec2.internal>
2026-04-21 23:34:02 +0000

    fixed deploy script permission

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,8 @@ docker rm $(docker ps -aq) || true
 
 docker pull 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
 
+<<<<<<< HEAD
 docker run -d -p 80:80 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
+=======
+docker run -d -p 80:80 --restart=always 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
+>>>>>>> 27557fc (fixed deploy script permission)
