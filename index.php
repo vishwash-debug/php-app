769b448360d07ed71ce19d658bbf8392f6cea1b3
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-21 18:56:13 +0530

    Add basic user input form with database connection

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
+<?php
+$host = "YOUR_RDS_ENDPOINT";
+$user = "admin";
+$pass = "password";
+$db = "testdb";
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $conn->query("INSERT INTO users (name) VALUES ('$name')");
+}
+
+$result = $conn->query("SELECT * FROM users");
+?>
+
+<form method="POST">
+    Name: <input type="text" name="name">
+    <button type="submit">Submit</button>
+</form>
+
+<?php while($row = $result->fetch_assoc()) { ?>
+    <p><?php echo $row['name']; ?></p>
+<?php } ?>
