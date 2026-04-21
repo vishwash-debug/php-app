9c0f844e000c9511c88200ed71f6b58474c1850c
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-22 04:56:04 +0530

    Update deploy script to stop and remove all containers

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker stop php-container || true
-docker rm php-container || true
+docker stop $(docker ps -q) || true
+docker rm $(docker ps -aq) || true
 
 docker pull 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
 
-docker run -d -p 80:80 --name php-container 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
+docker run -d -p 80:80 319332498304.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
